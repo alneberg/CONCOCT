c55d4890d5b93f25012625bb7ed1e48f394f381e
Johannes Alneberg <alneberg@kth.se>
2018-09-14 15:29:59 +0200

    Replacing deprecated pngmath

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinxcontrib.programoutput',
 ]
 
